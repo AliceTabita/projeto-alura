844f554c87d7ae5eb7d4b89a1806c5c6530680ac
alice <alicetabita26@gmail.com>
2024-10-22 13:29:43 -0300

    feat: incluindo cadastro de cursos

--- src/main/java/br/com/alura/ProjetoAlura/course/CourseController.java
+++ src/main/java/br/com/alura/ProjetoAlura/course/CourseController.java
@@ -1,5 +1,8 @@
 package br.com.alura.ProjetoAlura.course;
 
+import br.com.alura.ProjetoAlura.user.Role;
+import br.com.alura.ProjetoAlura.user.User;
+import br.com.alura.ProjetoAlura.user.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
+    private final CourseRepository courseRepository;
+    private final UserRepository userRepository;
+
+    public CourseController(CourseRepository courseRepository, UserRepository userRepository) {
+        this.courseRepository = courseRepository;
+        this.userRepository = userRepository;
+    }
 
     @PostMapping("/course/new")
     public ResponseEntity createCourse(@Valid @RequestBody NewCourseDTO newCourse) {
-        // TODO: Implementar a Questão 1 - Cadastro de Cursos aqui...
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        Course course = newCourse.toModel();
+        User user= userRepository.findByEmail(newCourse.getInstructorEmail());
+        if(user == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Instrutor não cadastrado");
+        }else if(!user.getRole().equals(Role.INSTRUCTOR)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O usuário não é um instrutor");
+        }else if(courseRepository.existsByCode(newCourse.getCode())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Já existe um curso cadastrado com esse código");
+        }
+        course.setInstructorId(user.getId());
+        courseRepository.save(course);
+        return ResponseEntity.status(HttpStatus.CREATED).body(course);
     }
 
     @PostMapping("/course/{code}/inactive")
