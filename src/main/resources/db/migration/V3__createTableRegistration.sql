1a8cc2ccee42ba96a0ac971a65431b1ad99262aa
alice <alicetabita26@gmail.com>
2024-10-22 15:11:42 -0300

    fix: alterando de instrutctorId para userId

--- src/main/resources/db/migration/V3__createTableRegistration.sql
+++ src/main/resources/db/migration/V3__createTableRegistration.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Registration(
                              id bigint(20) NOT NULL AUTO_INCREMENT,
                              registrationDate datetime,
-                             instructorId bigint(20) NOT NULL,
+                             userId bigint(20) NOT NULL,
                              courseId bigint(20) NOT NULL,
                              PRIMARY KEY (id),
-                             FOREIGN KEY (instructorId) REFERENCES User (id),
+                             FOREIGN KEY (userId) REFERENCES User (id),
                              FOREIGN KEY (courseId) REFERENCES Course (id)
 )
